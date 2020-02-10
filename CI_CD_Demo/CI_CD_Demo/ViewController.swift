2f3b8434b2f27d7b072319f02438a12026b9cc49
Anshul Bansal <anshubansal@deloitte.com>
2020-02-10 17:56:36 +0530

    Initial Commit

---
+++ CI_CD_Demo/CI_CD_Demo/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CI_CD_Demo
+//
+//  Created by Bansal, Anshul on 10/02/20.
+//  Copyright © 2020 Deloitte Digital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var lblCaughtMe: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func btnCatchTapped(_ sender: UIButton) {
+        lblCaughtMe.isHidden = false
+    }
+
+}
+
